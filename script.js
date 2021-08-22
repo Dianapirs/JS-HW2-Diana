09d053f3200499a0bdb76919ec7f1845529984fb
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-22 22:33:23 +0300

    First task

---
+++ script.js
@@ -0,0 +1,11 @@
+// First task
+console.log('First task');
+let name = prompt('Ваше имя');
+let age = prompt('Ваш возраст');
+let city = prompt('Город проживания');
+let phone = prompt('Номер телефона');
+let email = prompt('Адрес электронной почты');
+let company = prompt('Место работы');
+
+console.log('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + '  и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
+document.write('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + '  и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
