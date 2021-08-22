72dda62fcd505f154769491b20c7bee07314454a
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-22 23:12:22 +0300

    Forth task

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ let str = '465393';
 ((Number(str[0]) + Number(str[1]) + Number(str[2])) == (Number(str[3]) + Number(str[4]) + Number(str[5]))) ? console.log('Да') : console.log('Нет');
 console.log('');
 
+//Forth task
+console.log('Forth task');
+let a = 1;
+(a > 0) ? console.log('Верно') : console.log('Неверно');
+    a = 0;
+(a > 0) ? console.log('Верно') : console.log('Неверно');
+    a = -3;
+(a > 0) ? console.log('Верно') : console.log('Неверно');
+console.log('');
