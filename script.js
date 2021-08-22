bfc8a5535577bf880a06c80fc49136bd00e5d041
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-22 22:47:02 +0300

    Second task

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ let company = prompt('Место работы');
 
 console.log('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + '  и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
 document.write('Меня зовут ' + name + '. Мне ' + age + ' лет. Я проживаю в городе ' + city + '  и работаю в компании ' + company + '. Мои контактные данные: ' + phone + ', ' + email + '.');
+console.log('');
+
+// Second task
+console.log('Second task');
+let yearOfBirth = 2021 - age;
+console.log(name + ' родился(лась) в ' + yearOfBirth + ' году.');
+document.write(name + ' родился(лась) в ' + yearOfBirth + ' году.');
