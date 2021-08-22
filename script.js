c8c7884f2b0c9d2db6c5d5dd263f83a3f09eecfd
Diana <89095339+Dianapirs@users.noreply.github.com>
2021-08-22 23:55:33 +0300

    Seventh task

--- script.js
+++ script.js
@@ -43,10 +43,26 @@ console.log('Сумма равна ' + (a + b) + '. Разность равна 
 console.log('');
 
 // Sixth task
-console.log('sixth task');
+console.log('Sixth task');
 if ((a > 2 && a < 11) || (b >= 6 && b < 14)) {
     console.log('Верно!');
 } else {
     console.log('Неверно!');
+}
+console.log('');
+
+// Seventh task
+console.log('Seventh task');
+let n = 32; //число от 0 до 59
+if (n >= 0 && n < 15) {
+    console.log(n + ' находится в первой четверти часа');
+} else if (n >= 15 && n < 30) {
+    console.log(n + ' находится во второй четверти часа');
+} else if (n >= 30 && n < 45) {
+    console.log(n + ' находится в третьей четверти часа');
+} else if (n >= 45 && n <= 59 ) {
+    console.log(n + ' находится в четвертой четверти часа');
+} else {
+    console.log('Число превышает количество минут');
 }
 console.log('');
